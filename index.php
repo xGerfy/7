9ffbb1a64999377f1d512671a8aec1d7960472bc
Roman Ponomarev <roman.ponomarev66@gmail.com>
2022-11-30 18:19:16 +0400

    feat: add seach function and text files

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <?php
+
+$searchRoot = __DIR__;
+$searchName = 'test.txt';
+$searchResult = [];
+
+function searchFile($searchRoot, $searchName, &$searchResult)
+{
+    foreach (scandir($searchRoot) as $item) {
+        if (is_dir($searchRoot . '/' . $item) && !($item == '.' || $item == '..')) {
+            searchFile($searchRoot . '/' . $item, $searchName, $searchResult);
+        } elseif ($item == $searchName) {
+            $searchResult[] = $searchRoot . '/' . $item;
+        }
+    }
+}
+
+searchFile($searchRoot,  $searchName, $searchResult);
+
+if (empty($searchResult)) {
+    echo 'Файл не найден';
+} else {
+    $searchResult = array_filter($searchResult, fn ($item) => filesize($item) > 0);
+    var_dump($searchResult);
+}
